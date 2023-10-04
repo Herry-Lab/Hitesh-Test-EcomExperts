2c0f390b569533cf0668bf28ce8fce4db7c898e3
Herry-Lab <satasiyahitesh@gmail.com>
2023-10-04 17:42:06 +0530

    Done all task

--- assets/product-form.js
+++ assets/product-form.js
@@ -112,3 +112,71 @@ if (!customElements.get('product-form')) {
     }
   );
 }
+
+// “Leather Bag” -with variant options “Black” & “Medium ” is added to the cart. The “Soft Winter Jacket” product will also be automatically added to the cart.
+// self executable function after document load
+(function() {
+
+  // Listen for cart update events
+
+  const unsubscribeListener = subscribe(PUB_SUB_EVENTS.cartUpdate, async ({cartData}) => {
+      // add bundle product if master is there and bundle product is not there.
+
+      const masterVariantId = 47096227987730;
+      const bundleProductId = 8774958317842;
+      const bundleProductVariantId = 47078806683922;
+
+      const masterProductAdded = cartData.id === masterVariantId;
+      const cartItems = await fetch("/cart.json").then(res => res.json());
+      const hasBundleProduct = cartItems.items.some(item => item.product_id === bundleProductId);
+
+      // check if our condition is met
+
+      if (masterProductAdded && !hasBundleProduct) {
+
+        const cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+        const config = fetchConfig('javascript');
+        config.headers['X-Requested-With'] = 'XMLHttpRequest';
+        delete config.headers['Content-Type'];
+
+        const formData = new FormData();
+        formData.append('id', bundleProductVariantId);
+        formData.append('product-id', bundleProductId);
+        formData.append('quantity', 1);
+        formData.append('form_type', 'product');
+
+        if (cart) {
+          formData.append(
+            'sections',
+            cart.getSectionsToRender().map((section) => section.id)
+          );
+          formData.append('sections_url', window.location.pathname);
+          cart.setActiveElement(document.activeElement);
+        }
+
+        config.body = formData;
+
+        fetch(`${routes.cart_add_url}`, config)
+          .then((response) => response.json())
+          .then((response) => {
+            publish(PUB_SUB_EVENTS.cartUpdate, { source: 'product-form', productVariantId: formData.get('id'), cartData: response });
+            if (cart) {
+              cart.renderContents(response);
+            }
+          })
+          .catch((e) => {
+            console.error(e);
+          })
+          .finally(() => {
+            // adding done.
+          });
+      }
+    });
+
+    // unsubscribe on unload
+
+    addEventListener('beforeunload', (event) => {
+       unsubscribeListener();
+    });
+
+})();
